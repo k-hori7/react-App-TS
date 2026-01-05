ee28dfc3b73ca22d74bfb3b2ec18adb9e8855c6e
k-hori7 <kouta.kapibara@gmail.com>
2026-01-05 17:43:28 +0900

    kadai - tailwindcssへの書き換え - Next.jsへの書き替え

---
+++ chapter-8/src/app/_components/PostCard.tsx
@@ -0,0 +1,41 @@
+"use client";
+import Link from "next/link";
+import type { DataPost } from "../type";
+
+export default function PostCard({ post }: DataPost) {
+  return (
+    <Link href={`/posts/${post.id}`} className="block ">
+      <article className="border border-gray-300 mb-8 p-4">
+        <div className="flex justify-between">
+          <div className="text-gray-500 text-sm">
+            {new Date(post.createdAt).toLocaleDateString("ja-JP")}
+          </div>
+          <div className="flex">
+            {post.categories.map((category: string, index: number) => (
+              <div
+                key={index}
+                className="border border-blue-600 text-blue-600 rounded px-2 py-0.5 text-sm mr-2"
+              >
+                {category}
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <p className="text-2xl mt-2 mb-4">APIで取得した{post.title}</p>
+
+        <div
+          className="text-gray-700"
+          style={{
+            display: "-webkit-box",
+            WebkitBoxOrient: "vertical",
+            WebkitLineClamp: 2,
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+          }}
+          dangerouslySetInnerHTML={{ __html: post.content }}
+        />
+      </article>
+    </Link>
+  );
+}
---
+++ chapter-8/src/app/posts/[id]/page.js
@@ -0,0 +1,9 @@
+import Detail from "../../_components/Detail";
+
+export default function Home() {
+  return (
+    <main>
+      <Detail />
+    </main>
+  );
+}
